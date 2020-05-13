8f0b4739a5505c2dce442b439a28355161651bcd
janelle mellor <janellemellor@gmail.com>
2020-05-13 14:51:56 -0700

    add test for post route

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -18,5 +18,27 @@ describe('book routes', () => {
     return mongoose.connection.close();
   });
 
-  
+  it('creates a book', async() => {
+    return request(app)
+      .post('/api/v1/books')
+      .send({
+        author: 'Nayyirah Waheed',
+        title: 'Salt',
+        genre: 'Poetry',
+        imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1380105266l/18585282.jpg',
+        synopsis: 'It was only and ever love'
+      })
+      .then(res => {
+          expect(res.body).toEqual({
+            _id: expect.any(String),
+            author: 'Nayyirah Waheed',
+            title: 'Salt',
+            genre: 'Poetry',
+            imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1380105266l/18585282.jpg',
+            synopsis: 'It was only and ever love',
+            __v: 0
+          });
+      });
+  });
+
 });
